b207ee54f5ff4bf7e9d078e8fef5d0a190122457
shelbyness <peepieev@gmail.com>
2025-01-09 01:16:22 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ const accept = () => {
 </script>
 <template>
   <div v-if="step === 1" class="container">
-    <h1>Gostaria de saber se você quer sair comigo!! hahaha 🥰</h1>
+    <h1>Oi, meu bem. Você quer sair comigo? ❤️</h1>
     <div class="buttons">
       <button class="accept-button" @click="accept">Sim</button>
       <button
@@ -49,8 +49,8 @@ const accept = () => {
     </div>
   </div>
   <div v-if="step === 2" class="container">
-    <h2>Eu sabia que você não iria resistir, hahahah!</h2>
-    <h3>Mandarei mais informações no whatsapp! Até breve!!! 🥰</h3>
+    <h2>Como se você tivesse escolha...</h2>
+    <h3>Agora dê seu jeito e escolha pra onde iremos, e mande mensagem no Telegram! 😁</h3>
 
   </div>
 </template>
